98bd96ec9ead2b940504570ce5d4ddfc15db21e7
Marat Abrarov <abrarov@gmail.com>
2020-03-19 23:42:00 +0300

    Task 2

---
+++ src/task2/src/main.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+
+#if defined(WIN32)
+#include <tchar.h>
+#endif
+
+#if defined(WIN32)
+int _tmain() {
+#else
+int main() {
+#endif
+  int max_seven_mult = 0, max_non_seven_mult = 0;
+  while (true) {
+    int n;
+    std::cin >> n;
+    if (0 == n) {
+      break;
+    }
+    if (n < 0) {
+      std::cerr << "Only non-negative integer numbers allowed. Got: " << n << std::endl;
+      return EXIT_FAILURE;
+    }
+    if (n % 7 == 0) {
+      if (n % 49 == 0) {
+        continue;
+      }
+      max_seven_mult = (std::max)(max_seven_mult, n);
+    }
+    max_non_seven_mult = (std::max)(max_non_seven_mult, n);
+  }
+  if (0 == max_seven_mult || 0 == max_non_seven_mult) {
+    std::cout << 1;
+  } else {
+    std::cout << max_seven_mult * max_non_seven_mult;
+  }
+  return EXIT_SUCCESS;
+}
